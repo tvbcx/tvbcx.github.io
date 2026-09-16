4fb8d1f412a907bcc4b4fd40ac58967f945c1159
tvbcx <wsaiiifx@gmail.com>
2026-09-16 10:26:31 +0100

    Update bio.js

--- bio.js
+++ bio.js
@@ -1,1 +1,1 @@
-window.PROFILE_BIO = `yield the yellow or push thru?<br><a href="https://gcaif.github.io/media/" class="bio-link"><strong>gcaif/media</strong></a>`;
+window.PROFILE_BIO = `yield the yellow or push thru?<br><a href="https://gcaif.github.io/media/" class="bio-link"><strong>gcaif.io/socials</strong></a>`;
