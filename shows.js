820a3aa888c1bd97df36f7819a68510a00f7c802
tvbcx <wsaiiifx@gmail.com>
2026-09-22 07:40:52 +0100

    Update shows.js

--- shows.js
+++ shows.js
@@ -46,7 +46,7 @@ var SHOWS = [
   { title: "The Last of Us",                     imdb: "tt3581920",  src: "https://static.tvmaze.com/uploads/images/medium_portrait/563/1409008.jpg",       rating: 3.5  , journalYear: 2025, journalOrder: 26 },
   { title: "Dune: Prophecy",                     imdb: "tt10466872", src: "https://static.tvmaze.com/uploads/images/medium_portrait/543/1358056.jpg",       rating: 3.5  , journalYear: 2025, journalOrder: 27 },
   { title: "Adolescence",                        imdb: "tt31806037", src: "https://static.tvmaze.com/uploads/images/medium_portrait/558/1395109.jpg",       rating: 4.5  , journalYear: 2025, journalOrder: 28 },
-  { title: "Those Who Read the Hearts of Evil",  imdb: "tt15007172", src: "https://static.tvmaze.com/uploads/images/medium_portrait/389/972574.jpg",        rating: 3.5  , journalYear: 2026, journalOrder: 30 },
+  { title: "Through the Darkness",               imdb: "tt15007172", src: "https://static.tvmaze.com/uploads/images/medium_portrait/389/972574.jpg",        rating: 3.5  , journalYear: 2026, journalOrder: 30 },
   { title: "Silo",                               imdb: "tt14688458",  src: "https://static.tvmaze.com/uploads/images/medium_portrait/631/1577677.jpg",       rating: 4  , journalYear: 2026, journalOrder: 31, seasons: [3] },
 
   // DNF
